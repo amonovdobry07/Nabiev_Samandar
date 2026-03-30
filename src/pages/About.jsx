388e4e4cb766ad31ec8830d91257513fd22c52fb
Oga'bek Amonov <amonovdobry07@gmail.com>
2026-03-30 13:27:07 +0500

    about changed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,7 +13,6 @@ export default function About() {
   return (
     <main className="aboutPage" aria-label={t("about_page_aria")}>
       <section className="aboutWrap">
-        <h1>Salom Qalesila Yaxshimisla </h1>
         <div className="aboutContainer">
           {/* LEFT: PHOTO */}
           <div className="aboutLeft" aria-label={t("about_photo_aria")}>
